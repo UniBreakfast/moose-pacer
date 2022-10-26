d287a36c7ad1e909f330f6284abdb0ae55835ceb
UniBreakfast <unibreakfast1@gmail.com>
2022-10-26 15:54:04 +0300

    add user list

--- js/auth/src/admin-modal.js
+++ js/auth/src/admin-modal.js
@@ -1,17 +1,49 @@
 export const adminModal = {
-  show() { modal.showModal() },
+  show() {
+    prepareForm()
+    modal.showModal()
+  },
+
   hide() { modal.close() },
 }
 
 import { makeModal } from './modal.js'
 import { makeForm } from './form.js'
 import { descriptor } from './admin-form.js'
+import { getUsers } from './get-users.js'
 
 const modal = makeModal()
 const adminForm = makeForm(descriptor)
 
 modal.append(adminForm)
 
-adminForm.onsubmit = async () => {
-  
+
+prepareForm()
+
+async function prepareForm() {
+  const tempList = await buildTempList()
+
+  adminForm.querySelector('.list-placeholder, .user-list')
+    .replaceWith(tempList)
+}
+
+async function buildTempList() {
+  const list = document.createElement('ul')
+  const users = await getUsers()
+
+  list.className = 'user-list'
+  list.append(...users.map(buildUserItem))
+
+  return list
+}
+
+function buildUserItem(user) {
+  const item = document.createElement('li')
+  const nameSpan = document.createElement('span')
+
+  nameSpan.textContent = user.name
+
+  item.append(nameSpan)
+
+  return item
 }
